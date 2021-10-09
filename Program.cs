eea59997f4e6f9a61c680bb3d40505706668a304
GGG KILLER <gggkiller2@gmail.com>
2021-10-09 01:12:42 -0300

    Remove useless code.

--- Program.cs
+++ Program.cs
@@ -1,41 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Operations;
 using RoslynFastStringSwitchPoc;
-
-//const string code = @"public class MyClass
-//{
-//    public bool IsKnownContentType(string contentTypeValue)
-//    {
-//        switch (contentTypeValue)
-//        {
-//            case ""text/xml"":
-//            case ""text/css"":
-//            case ""text/csv"":
-//            case ""image/gif"":
-//            case ""image/png"":
-//            case ""text/html"":
-//            case ""text/plain"":
-//            case ""image/jpeg"":
-//            case ""application/pdf"":
-//            case ""application/xml"":
-//            case ""application/zip"":
-//            case ""application/grpc"":
-//            case ""application/json"":
-//            case ""multipart/form-data"":
-//            case ""application/javascript"":
-//            case ""application/octet-stream"":
-//            case ""text/html; charset=utf-8"":
-//            case ""text/plain; charset=utf-8"":
-//            case ""application/json; charset=utf-8"":
-//            case ""application/x-www-form-urlencoded"":
-//                return true;
-//        }
-//        return false;
-//    }
-//}";
 
 string uriOrPath;
 if (args.Length == 1)
